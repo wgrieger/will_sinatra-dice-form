9d1739dc8dab3aa5a95d1bc3d29d01460527ea1a
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-17 00:21:09 +0000

    working

--- app.rb
+++ app.rb
@@ -6,13 +6,15 @@ get("/") do
 end
 
 get("/dice/sides") do
-  :num_dice.times do 
-    roll_output=Array.new
-    roll= rand(1...:num_sides)
-    roll_output.push(roll)
+  @dice=params.fetch(:num_dice).to_i
+  @sides=params.fetch(:num_sides).to_i
+  
+  @roll_output=Array.new
+  @dice.times do 
+    roll= rand(1...@sides)
+    @roll_output.push(roll)
   end 
 
-roll_output.each do |show|
-  
+erb(:roll)
 
 end
---
+++ test.rb
@@ -0,0 +1,1 @@
+pp @roll_output
