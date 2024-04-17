d1c413d6dc47cf37a6c1c3e3af76b866851c78f6
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-17 01:43:44 +0000

    all_done

--- app.rb
+++ app.rb
@@ -5,9 +5,9 @@ get("/") do
  erb (:home)
 end
 
-get("/dice/sides") do
-  @dice=params.fetch(:num_dice).to_i
-  @sides=params.fetch(:num_sides).to_i
+get("/process_roll") do
+  @dice=params.fetch(:dice).to_i
+  @sides=params.fetch(:sides).to_i
   
   @roll_output=Array.new
   @dice.times do 
