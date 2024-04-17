d82d74f910c3ba7f8a28e65492a01b38535fb49b
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-17 00:06:26 +0000

    in_progress

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+ erb (:home)
+end
+
+get("/dice/sides") do
+  :num_dice.times do 
+    roll_output=Array.new
+    roll= rand(1...:num_sides)
+    roll_output.push(roll)
+  end 
+
+roll_output.each do |show|
+  
+
 end
